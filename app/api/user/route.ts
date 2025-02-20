40a1d45392398b4dd7c42ad404e659d5fd376f5e
Manobhav Sachan <manobhavsachan@gmail.com>
2025-02-20 11:29:23 +0530

    Refactor user API route and extract getUser helper function
    - Move getUser function to a separate helper file
    - Update import statements in project route to use the new helper location
    - Simplify user route by importing the extracted helper function

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,29 +1,5 @@
-import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
-import { prisma } from "@/lib/prismaClient";
-
-export const getUser = async () => {
-  const { userId: clerkId } = await auth();
-  if (!clerkId) {
-    throw new Error("Unauthorized");
-  }
-
-  const user = await prisma.user.findUnique({
-    where: {
-      externalId: clerkId,
-    },
-    select: {
-      id: true,
-    },
-  });
-
-  if (!user) {
-    throw new Error("User not found");
-  }
-
-  return user;
-};
-
+import { getUser } from "./helper";
 export async function GET() {
   try {
     const user = await getUser();
